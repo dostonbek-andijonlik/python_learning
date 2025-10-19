fe7a115bea09594b1b8e5ae7a3bc55a1cccb8616
dostonbek.andijonlik <dostonbek.andijonlik@gmail.com>
2025-10-19 19:44:34 +0500

    string indexing

---
+++ day1/string_indexing.py
@@ -0,0 +1,14 @@
+# indexing = accesing elements of a sequence using [] (indexing operator)
+# [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[0])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[::5])
+# print(credit_number[-4:])
+
+reverse = credit_number[::-1]
+print(reverse)
