0954dd79970351ee0740a738fa8d547724ea2425
dostonbek.andijonlik <dostonbek.andijonlik@gmail.com>
2025-10-19 20:03:20 +0500

    do format specifiers

---
+++ day1/format_specifiers.py
@@ -0,0 +1,9 @@
+# format specifiers = {value : flags} format a value based on what flags are inserted
+
+price1 = 3.141534
+price2 = 9.43202
+price3 = -12.45323
+
+print(f"Price 1 is {price1:^10}")
+print(f"Price 2 is {price2:^10}")
+print(f"Price 3 is {price3:^10}")
