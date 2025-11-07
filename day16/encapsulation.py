181623b91db3474bd7e7337eddd9c9da19593b0f
Dostonbek <100673731+dostonbek-andijonlik@users.noreply.github.com>
2025-11-07 14:47:20 +0500

    Add files via upload

---
+++ day16/encapsulation.py
@@ -0,0 +1,34 @@
+# class Person:
+#     def __init__(self, name, age):
+#         self.__age = age
+#         self.name = name
+    
+# p1 = Person("Marcus", 32)
+# # # print(p1.__age)
+# # print(p1.name)
+
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.__age = age
+    
+#     def get_age(self):
+#         return self.__age
+    
+# p1 = Person("Robert", 32)
+# print(p1.get_age())
+# print(p1.name)
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.__age = age
+#         self.name = name
+    
+#     def get_age(self):
+#         return self.__age
+
+# p1 = Person("Federico", 32)
+# print(p1.get_age())
+# print(p1.name)
